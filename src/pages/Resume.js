59fad96a10425a1102dec5eddefd4222efca4ada
cquentin26200 <77721932+cquentin26200@users.noreply.github.com>
2022-09-27 21:58:14 +0200

    finish header for desktop version

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import Navigation from '../components/Navigation';
+import React from "react";
+import Navigation from "../components/Navigation";
 
 const Resume = () => {
-    return (
-        <div>
-            <header>
-                <Navigation />
-            </header>
-        </div>
-    );
+  return (
+    <div>
+      <div id="principal-page">
+        <header>
+          <h1>résumé</h1>
+          <Navigation />
+        </header>
+        <main>
+            <p className="more-information">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque at molestias sequi, harum numquam enim ex alias pariatur! Ipsa deserunt in labore! Deleniti et suscipit dolorem id exercitationem aspernatur placeat.
+            Facilis similique exercitationem necessitatibus? Dolorem, accusantium quas consectetur atque esse enim eveniet nobis, unde rem inventore itaque cupiditate, recusandae laudantium nulla quaerat illum et id aliquam dolore fuga reiciendis magnam.</p>
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default Resume;
